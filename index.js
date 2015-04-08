3c9158c24b8a689b835fa139282494fee19c0bc4
Thomas Watson Steen <w@tson.dk>
2015-04-08 01:15:51 +0100

    GitHub is spelled Github

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var pkg = require('./package')
 
 var userAgent = pkg.name + '/' + pkg.version
 var css = fs.readFileSync(path.join(__dirname, 'style.css'))
-var head = '<!doctype html><head><meta charset=utf-8><title>GitHub followers</title><style type="text/css">' + css + '</style></head><body><div id=container>'
+var head = '<!doctype html><head><meta charset=utf-8><title>Github followers</title><style type="text/css">' + css + '</style></head><body><div id=container>'
 var top10k = []
 
 debug('Loading top 10k Github users')
@@ -26,7 +26,7 @@ fs.createReadStream(path.join(__dirname, 'top-10K.csv'))
   })
 
 var form = function (login, avatar, rank) {
-  return '<form onsubmit="window.location = document.getElementById(\'username\').value; return false"><input type=text name=username id=username placeholder="Enter GitHub username"> <input type=submit value=View></form>'
+  return '<form onsubmit="window.location = document.getElementById(\'username\').value; return false"><input type=text name=username id=username placeholder="Enter Github username"> <input type=submit value=View></form>'
 }
 
 var userDiv = function (login, avatar, rank) {
@@ -88,7 +88,7 @@ patterns.add('GET /{username}', function (req, res) {
     body.push('</div>')
     body.push(form())
     body.push('</div>')
-    body.push('<a href="https://github.com/watson/github-followers"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>')
+    body.push('<a href="https://github.com/watson/github-followers"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on Github" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>')
     body = body.join('\n')
 
     res.writeHead(200, {
