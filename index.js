05080add6d40682c9e92aec6793e4cca96964914
Thomas Watson Steen <w@tson.dk>
2015-04-08 13:01:47 +0100

    Turns out it's actually spelling GitHub

--- index.js
+++ index.js
@@ -12,23 +12,23 @@ var pkg = require('./package')
 
 var userAgent = pkg.name + '/' + pkg.version
 var css = fs.readFileSync(path.join(__dirname, 'style.css'))
-var head = '<!doctype html><head><meta charset=utf-8><title>Github followers</title><style type="text/css">' + css + '</style></head><body><div id=container>'
-var foot = '<a href="https://github.com/watson/github-followers"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on Github" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>' +
+var head = '<!doctype html><head><meta charset=utf-8><title>GitHub followers</title><style type="text/css">' + css + '</style></head><body><div id=container>'
+var foot = '<a href="https://github.com/watson/github-followers"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>' +
   '<script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\'); ga(\'create\', \'' + process.env.GA_TRACKING_ID + '\', \'auto\'); ga(\'send\', \'pageview\'); </script>'
 var top10k = []
 
-debug('Loading top 10k Github users')
+debug('Loading top 10k GitHub users')
 fs.createReadStream(path.join(__dirname, 'top-10K.csv'))
   .pipe(csv())
   .on('data', function (data) {
     top10k.push(data.login)
   })
   .on('end', function () {
-    debug('Finished loading top 10k Github users')
+    debug('Finished loading top 10k GitHub users')
   })
 
 var form = function (login, avatar, rank) {
-  return '<form onsubmit="window.location = document.getElementById(\'username\').value; return false"><input type=text name=username id=username placeholder="Enter Github username"> <input type=submit value=View></form>'
+  return '<form onsubmit="window.location = document.getElementById(\'username\').value; return false"><input type=text name=username id=username placeholder="Enter GitHub username"> <input type=submit value=View></form>'
 }
 
 var userDiv = function (login, avatar, rank) {
@@ -58,7 +58,7 @@ patterns.add('GET /{username}', function (req, res) {
     if (!Array.isArray(data)) {
       res.writeHead(404)
       res.write(head)
-      res.write('Could not find ' + username + ' on Github')
+      res.write('Could not find ' + username + ' on GitHub')
       res.write(form())
       res.write(foot)
       res.end()
@@ -80,8 +80,8 @@ patterns.add('GET /{username}', function (req, res) {
 
     var body = [head]
     body.push(userDiv(username, 'https://github.com/' + username + '.png', rank))
-    body.push('<h2>Top Github followers</h2>')
-    body.push('<p>These people follow ' + username + ' and are all among the top 10k most active Github users in the world</p>')
+    body.push('<h2>Top GitHub followers</h2>')
+    body.push('<p>These people follow ' + username + ' and are all among the top 10k most active GitHub users in the world</p>')
     body.push('<div id=followers>')
 
     data.forEach(function (user) {
@@ -89,7 +89,7 @@ patterns.add('GET /{username}', function (req, res) {
       body.push(userDiv(user.login, user.avatar_url, rank))
     })
 
-    body.push('<div id=twitter><span><a href="https://twitter.com/share" class="twitter-share-button" data-text="Which top 10k most active Github follows you? These follow me:" data-size="large" data-count="none" data-dnt="true">Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+\'://platform.twitter.com/widgets.js\';fjs.parentNode.insertBefore(js,fjs);}}(document, \'script\', \'twitter-wjs\');</script></span></div>')
+    body.push('<div id=twitter><span><a href="https://twitter.com/share" class="twitter-share-button" data-text="Which top 10k most active GitHub follows you? These follow me:" data-size="large" data-count="none" data-dnt="true">Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+\'://platform.twitter.com/widgets.js\';fjs.parentNode.insertBefore(js,fjs);}}(document, \'script\', \'twitter-wjs\');</script></span></div>')
 
     if (!data.length) body.push('<p><strong>' + username + ' doesn\'t yet have any followers in top 10k</strong></p>')
 
