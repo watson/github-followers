5093667dbbdf428b47078645010c173e1efd22ec
Thomas Watson Steen <w@tson.dk>
2015-04-08 02:06:10 +0100

    Reuse footer over entire site

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ var pkg = require('./package')
 var userAgent = pkg.name + '/' + pkg.version
 var css = fs.readFileSync(path.join(__dirname, 'style.css'))
 var head = '<!doctype html><head><meta charset=utf-8><title>Github followers</title><style type="text/css">' + css + '</style></head><body><div id=container>'
+var foot = '<a href="https://github.com/watson/github-followers"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on Github" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>' +
+  '<script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\'); ga(\'create\', \'' + process.env.GA_TRACKING_ID + '\', \'auto\'); ga(\'send\', \'pageview\'); </script>'
 var top10k = []
 
 debug('Loading top 10k Github users')
@@ -34,7 +36,7 @@ var userDiv = function (login, avatar, rank) {
 }
 
 patterns.add('GET /', function (req, res) {
-  res.end(head + form())
+  res.end(head + form() + foot)
 })
 
 patterns.add('GET /{username}', function (req, res) {
@@ -57,7 +59,8 @@ patterns.add('GET /{username}', function (req, res) {
       res.writeHead(404)
       res.write(head)
       res.write('Could not find ' + username + ' on Github')
-      res(form())
+      res.write(form())
+      res.write(foot)
       res.end()
       return
     }
@@ -93,8 +96,7 @@ patterns.add('GET /{username}', function (req, res) {
     body.push('</div>')
     body.push(form())
     body.push('</div>')
-    body.push('<a href="https://github.com/watson/github-followers"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on Github" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>')
-    body.push('<script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\'); ga(\'create\', \'' + process.env.GA_TRACKING_ID + '\', \'auto\'); ga(\'send\', \'pageview\'); </script>')
+    body.push(foot)
     body = body.join('\n')
 
     res.writeHead(200, {
